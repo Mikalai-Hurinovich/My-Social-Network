bc439280289891f3b33d0c614118b9e50d2f8ccb
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-05-06 08:59:47 +0300

    feat: status onBlur & onDoubleClick

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -9,20 +9,34 @@ class ProfileStatus extends React.Component <ProfileStatusType> {
         editMode: false
     }
 
+    activateEditMode() {
+        // setState - асинхронная функция
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactivateEditMode() {
+        // setState - асинхронная функция
+        this.setState({
+            editMode: false
+        })
+    }
+
     render() {
         return <>
             {!this.state.editMode
                 ?
                 <div>
-                    <span>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
                 </div>
                 :
                 this.state.editMode &&
                 <div>
-                    <input value={this.props.status}/>
+                    <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
                 </div>
             }
         </>
     };
-};
+}
 export default ProfileStatus;
