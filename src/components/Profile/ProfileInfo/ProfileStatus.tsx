05451d3102c4721e798fb31e4b0ddf2704e1a46c
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-05-06 10:03:16 +0300

    feat: change functions to =>

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -9,14 +9,14 @@ class ProfileStatus extends React.Component <ProfileStatusType> {
         editMode: false
     }
 
-    activateEditMode() {
+    activateEditMode = () => {
         // setState - асинхронная функция
         this.setState({
             editMode: true
         })
     }
 
-    deactivateEditMode() {
+    deactivateEditMode = () => {
         // setState - асинхронная функция
         this.setState({
             editMode: false
@@ -28,15 +28,16 @@ class ProfileStatus extends React.Component <ProfileStatusType> {
             {!this.state.editMode
                 ?
                 <div>
-                    <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                 </div>
                 :
                 this.state.editMode &&
                 <div>
-                    <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                    <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/>
                 </div>
             }
         </>
     };
 }
+
 export default ProfileStatus;
