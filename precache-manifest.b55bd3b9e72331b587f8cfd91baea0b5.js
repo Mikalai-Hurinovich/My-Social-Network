0a1e5fdd5366132219ad3d170dd9ad9d09e329e4
Mikalai-Hurinovich <Gurinovich-ns@mail.ru>
2021-06-14 08:10:58 +0300

    Updates

---
+++ precache-manifest.b55bd3b9e72331b587f8cfd91baea0b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "75c64b13d2d6950db549",
+    "url": "/My-Social-Network/Profile/static/css/main.bfded48c.chunk.css"
+  },
+  {
+    "revision": "75c64b13d2d6950db549",
+    "url": "/My-Social-Network/Profile/static/js/main.75c64b13.chunk.js"
+  },
+  {
+    "revision": "ddd1a35d820c2f8deed3",
+    "url": "/My-Social-Network/Profile/static/js/1.ddd1a35d.chunk.js"
+  },
+  {
+    "revision": "96d1bcad0b5f924e4f82",
+    "url": "/My-Social-Network/Profile/static/js/runtime~main.96d1bcad.js"
+  },
+  {
+    "revision": "591d2cd5abc37ae14004bae77e56044e",
+    "url": "/My-Social-Network/Profile/static/media/anonymous.591d2cd5.jpg"
+  },
+  {
+    "revision": "962f67edae0c6648b487484531ed5a39",
+    "url": "/My-Social-Network/Profile/static/media/preloader.962f67ed.svg"
+  },
+  {
+    "revision": "53a3703bfabad5ba55b036607aa2c4c1",
+    "url": "/My-Social-Network/Profile/index.html"
+  }
+];
